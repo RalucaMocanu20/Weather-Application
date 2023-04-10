1f3d64cf1800ae492832d4d9af238f6fab43eb67
RalucaMocanu20 <127863472+RalucaMocanu20@users.noreply.github.com>
2023-04-10 13:12:45 +0300

    Add files via upload
    Weather App

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "272f8c71c2mshbbffe73740793a2p1007c5jsn625cc8f58be7",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "69f4ddae3eb89c3e6d6acf1fe6b2e0ad"; 
